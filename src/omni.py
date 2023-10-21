d4c1bfe299e4f2753bf7db53e290ea4a55ac356a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-21 20:16:16 -0300

    new method for get index datetime

--- src/omni.py
+++ src/omni.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import datetime as dt
 import numpy as np 
+import base as b 
 
-INDEX_PATH = 'database/indices/omni.txt'
 
-def dt2dttime(y, d, hour, minute):
-    dn = (dt.date(int(y), 1, 1) + dt.timedelta(int(d) - 1))
+INDEX_PATH = 'database/indices/omni_hourly.txt'
+FORMAT_PATH = 'database/indices/omni_format_hourly.txt'
+
+
+def dt2dttime(y, d, hour):
+    dn = (dt.date(int(y), 1, 1) + 
+          dt.timedelta(int(d) - 1))
     return dt.datetime(
         dn.year, 
         dn.month, 
         dn.day, 
-        int(hour), 
-        int(minute)
+        int(hour)
         )
 
 def doy2date(df):
     return (dt.date(int(df.year), 1, 1) + 
             dt.timedelta(int(df.doy) - 1))
 
-def set_names(
-        format_file =  'database/indices/omni_format.txt'
-        ):
+def set_names():
 
-    f = open(format_file).readlines()
+    f = open(FORMAT_PATH).readlines()
     
     names = {}
     for i in f:
@@ -53,10 +55,14 @@ def OMNI2(
         delim_whitespace = True
         )
     
-    df.index = df.apply(lambda x: doy2date(x), axis = 1)
+
+    df['date'] =  df.apply(lambda x: doy2date(x), axis = 1)
+        
     
-    df.replace((99999, 9999, 999.9), 
-               np.nan, inplace = True)
+    df.replace(
+        (99999, 9999, 999.9), 
+        np.nan, inplace = True
+        )
     
     df.rename(
         columns = {'f10.7': 'f107'}, 
@@ -72,20 +78,20 @@ def OMNI2(
     cols = [s.replace(',', '') for s in df.columns]
     
     df.columns =  cols
+        
+    df.index =  (pd.to_datetime(df['date']) +
+                 pd.to_timedelta(df['hour'], unit='h'))
     
-    del df['hour']
-
+    df.drop(
+        columns = ['year', 'doy', 'hour', 'date'], 
+        inplace = True
+        )
     return df
     
 
-
-
-infile = "database/indices/omni.txt"
-
-
 def process(infile):
     try:
-        names =  set_names().values()
+        names = set_names().values()
         ds = OMNI2(infile, names)
         
         ds.to_csv(infile)
@@ -95,4 +101,10 @@ def process(infile):
         pass
     
 
-# process(infile)
+# process(INDEX_PATH )
+
+
+# names = set_names().values()
+# ds = OMNI2(INDEX_PATH, names)
+
+# ds
